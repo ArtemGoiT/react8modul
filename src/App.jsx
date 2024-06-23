bfaf6e4f4c410736b5c4bb1a8cd341b077b95bbc
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-06-23 15:32:09 +0300

    add-jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
-import { Heading } from 'components';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { lazy } from 'react';
+import { Header } from './components';
+
+const Home = lazy(() => import("pages/Home"));
+const Rates = lazy(() => import("pages/Rates"));
+
 
 export const App = () => {
-  return <Heading title="Just do it!" />;
+  return (
+    <Routes>
+      <Route path='/' element={<Header/>}>
+      <Route index element={<Home />} />
+        <Route path="/rates" element={<Rates />} />
+        <Route path='*' element={<Navigate to='/'/>} />
+      </Route>     
+    </Routes>);
 };
